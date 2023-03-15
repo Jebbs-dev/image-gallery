f6402221e750565eac1e56f3904a62ad959fac59
Jebbs-dev <77011708+Jebbs-dev@users.noreply.github.com>
2023-03-15 17:26:02 +0100

    feat:(added isLoading logic)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import ImageCard from "./components/ImageCard";
 
+
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -19,14 +20,24 @@ function App() {
       .catch((error) => console.log(error));
   }, []);
 
+  let content = <h1>Gallery not loaded</h1>;
+
+  if (isLoading) {
+    content = <h1 className="text-6xl text-center mx-auto">Loading...</h1>;
+  } else {
+    content = (
+      <>
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </>
+    );
+  }
+
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-3 gap-4">
-        {images.map(image=>(
-          <ImageCard
-            key={image.id} image={image}
-          />
-        ))}
+        {content}
       </div>
     </div>
   );
